c281a736eb8673471a67fff384de76d21d5439cb
Mahdi MalekiFard <mmf1344py@gmail.com>
2025-08-01 15:22:45 +0330

    initial commit.

---
+++ config/broadcasting.php
@@ -0,0 +1,42 @@
+<?php
+
+return [
+
+    'default' => env('BROADCAST_DRIVER', 'null'),
+
+    'connections' => [
+
+        'pusher' => [
+            'driver' => 'pusher',
+            'key' => env('PUSHER_APP_KEY'),
+            'secret' => env('PUSHER_APP_SECRET'),
+            'app_id' => env('PUSHER_APP_ID'),
+            'options' => [
+                'host' => env('PUSHER_HOST', '127.0.0.1'),
+                'port' => env('PUSHER_PORT', 6001),
+                'scheme' => env('PUSHER_SCHEME', 'http'),
+                'useTLS' => env('PUSHER_SCHEME') === 'https',
+                'encrypted' => true,
+            ],
+        ],
+
+        'ably' => [
+            'driver' => 'ably',
+            'key' => env('ABLY_KEY'),
+        ],
+
+        'reverb' => [
+            'driver' => 'reverb',
+        ],
+
+        'log' => [
+            'driver' => 'log',
+        ],
+
+        'null' => [
+            'driver' => 'null',
+        ],
+
+    ],
+
+];
